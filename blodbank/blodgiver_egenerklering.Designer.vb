c2e051a8e57a9acdea517bbd9b98d9abad4474f1
noralervik <noralervik@gmail.com>
2017-03-13 11:56:44 +0100

    Egenerklæring
    Egenerklæring som ikke ble med på forrige commit.

--- blodbank/blodgiver_egenerklering.Designer.vb
+++ blodbank/blodgiver_egenerklering.Designer.vb
@@ -22,8 +22,202 @@ Partial Class blodgiver_egenerklering
     'Do not modify it using the code editor.
     <System.Diagnostics.DebuggerStepThrough()> _
     Private Sub InitializeComponent()
-        components = New System.ComponentModel.Container
+        Me.MenuStrip1 = New System.Windows.Forms.MenuStrip()
+        Me.MinSideToolStripMenuItem = New System.Windows.Forms.ToolStripMenuItem()
+        Me.TimebestillingToolStripMenuItem = New System.Windows.Forms.ToolStripMenuItem()
+        Me.EgenerklæringToolStripMenuItem = New System.Windows.Forms.ToolStripMenuItem()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.Label2 = New System.Windows.Forms.Label()
+        Me.RadioButton1 = New System.Windows.Forms.RadioButton()
+        Me.RadioButton2 = New System.Windows.Forms.RadioButton()
+        Me.RadioButton3 = New System.Windows.Forms.RadioButton()
+        Me.RadioButton4 = New System.Windows.Forms.RadioButton()
+        Me.Label3 = New System.Windows.Forms.Label()
+        Me.RadioButton5 = New System.Windows.Forms.RadioButton()
+        Me.RadioButton6 = New System.Windows.Forms.RadioButton()
+        Me.Label4 = New System.Windows.Forms.Label()
+        Me.Label6 = New System.Windows.Forms.Label()
+        Me.MenuStrip1.SuspendLayout()
+        Me.SuspendLayout()
+        '
+        'MenuStrip1
+        '
+        Me.MenuStrip1.ImageScalingSize = New System.Drawing.Size(24, 24)
+        Me.MenuStrip1.Items.AddRange(New System.Windows.Forms.ToolStripItem() {Me.MinSideToolStripMenuItem, Me.TimebestillingToolStripMenuItem, Me.EgenerklæringToolStripMenuItem})
+        Me.MenuStrip1.Location = New System.Drawing.Point(0, 0)
+        Me.MenuStrip1.Name = "MenuStrip1"
+        Me.MenuStrip1.Size = New System.Drawing.Size(734, 33)
+        Me.MenuStrip1.TabIndex = 2
+        Me.MenuStrip1.Text = "MenuStrip1"
+        '
+        'MinSideToolStripMenuItem
+        '
+        Me.MinSideToolStripMenuItem.Name = "MinSideToolStripMenuItem"
+        Me.MinSideToolStripMenuItem.Size = New System.Drawing.Size(93, 29)
+        Me.MinSideToolStripMenuItem.Text = "Min Side"
+        '
+        'TimebestillingToolStripMenuItem
+        '
+        Me.TimebestillingToolStripMenuItem.Name = "TimebestillingToolStripMenuItem"
+        Me.TimebestillingToolStripMenuItem.Size = New System.Drawing.Size(133, 29)
+        Me.TimebestillingToolStripMenuItem.Text = "Timebestilling"
+        '
+        'EgenerklæringToolStripMenuItem
+        '
+        Me.EgenerklæringToolStripMenuItem.Name = "EgenerklæringToolStripMenuItem"
+        Me.EgenerklæringToolStripMenuItem.Size = New System.Drawing.Size(137, 29)
+        Me.EgenerklæringToolStripMenuItem.Text = "Egenerklæring"
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.Location = New System.Drawing.Point(23, 62)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(113, 20)
+        Me.Label1.TabIndex = 3
+        Me.Label1.Text = "Egenerklæring"
+        '
+        'Label2
+        '
+        Me.Label2.AutoSize = True
+        Me.Label2.Location = New System.Drawing.Point(23, 104)
+        Me.Label2.Name = "Label2"
+        Me.Label2.Size = New System.Drawing.Size(55, 20)
+        Me.Label2.TabIndex = 4
+        Me.Label2.Text = "Spm 1"
+        '
+        'RadioButton1
+        '
+        Me.RadioButton1.AutoSize = True
+        Me.RadioButton1.Location = New System.Drawing.Point(124, 102)
+        Me.RadioButton1.Name = "RadioButton1"
+        Me.RadioButton1.Size = New System.Drawing.Size(51, 24)
+        Me.RadioButton1.TabIndex = 5
+        Me.RadioButton1.TabStop = True
+        Me.RadioButton1.Text = "Ja"
+        Me.RadioButton1.UseVisualStyleBackColor = True
+        '
+        'RadioButton2
+        '
+        Me.RadioButton2.AutoSize = True
+        Me.RadioButton2.Location = New System.Drawing.Point(196, 104)
+        Me.RadioButton2.Name = "RadioButton2"
+        Me.RadioButton2.Size = New System.Drawing.Size(57, 24)
+        Me.RadioButton2.TabIndex = 6
+        Me.RadioButton2.TabStop = True
+        Me.RadioButton2.Text = "Nei"
+        Me.RadioButton2.UseVisualStyleBackColor = True
+        '
+        'RadioButton3
+        '
+        Me.RadioButton3.AutoSize = True
+        Me.RadioButton3.Location = New System.Drawing.Point(196, 146)
+        Me.RadioButton3.Name = "RadioButton3"
+        Me.RadioButton3.Size = New System.Drawing.Size(57, 24)
+        Me.RadioButton3.TabIndex = 9
+        Me.RadioButton3.TabStop = True
+        Me.RadioButton3.Text = "Nei"
+        Me.RadioButton3.UseVisualStyleBackColor = True
+        '
+        'RadioButton4
+        '
+        Me.RadioButton4.AutoSize = True
+        Me.RadioButton4.Location = New System.Drawing.Point(124, 144)
+        Me.RadioButton4.Name = "RadioButton4"
+        Me.RadioButton4.Size = New System.Drawing.Size(51, 24)
+        Me.RadioButton4.TabIndex = 8
+        Me.RadioButton4.TabStop = True
+        Me.RadioButton4.Text = "Ja"
+        Me.RadioButton4.UseVisualStyleBackColor = True
+        '
+        'Label3
+        '
+        Me.Label3.AutoSize = True
+        Me.Label3.Location = New System.Drawing.Point(23, 146)
+        Me.Label3.Name = "Label3"
+        Me.Label3.Size = New System.Drawing.Size(55, 20)
+        Me.Label3.TabIndex = 7
+        Me.Label3.Text = "Spm 1"
+        '
+        'RadioButton5
+        '
+        Me.RadioButton5.AutoSize = True
+        Me.RadioButton5.Location = New System.Drawing.Point(197, 187)
+        Me.RadioButton5.Name = "RadioButton5"
+        Me.RadioButton5.Size = New System.Drawing.Size(57, 24)
+        Me.RadioButton5.TabIndex = 12
+        Me.RadioButton5.TabStop = True
+        Me.RadioButton5.Text = "Nei"
+        Me.RadioButton5.UseVisualStyleBackColor = True
+        '
+        'RadioButton6
+        '
+        Me.RadioButton6.AutoSize = True
+        Me.RadioButton6.Location = New System.Drawing.Point(125, 185)
+        Me.RadioButton6.Name = "RadioButton6"
+        Me.RadioButton6.Size = New System.Drawing.Size(51, 24)
+        Me.RadioButton6.TabIndex = 11
+        Me.RadioButton6.TabStop = True
+        Me.RadioButton6.Text = "Ja"
+        Me.RadioButton6.UseVisualStyleBackColor = True
+        '
+        'Label4
+        '
+        Me.Label4.AutoSize = True
+        Me.Label4.Location = New System.Drawing.Point(24, 187)
+        Me.Label4.Name = "Label4"
+        Me.Label4.Size = New System.Drawing.Size(55, 20)
+        Me.Label4.TabIndex = 10
+        Me.Label4.Text = "Spm 1"
+        '
+        'Label6
+        '
+        Me.Label6.AutoSize = True
+        Me.Label6.Location = New System.Drawing.Point(383, 150)
+        Me.Label6.Name = "Label6"
+        Me.Label6.Size = New System.Drawing.Size(200, 20)
+        Me.Label6.TabIndex = 14
+        Me.Label6.Text = "Skal vi bruke radiobuttons?"
+        '
+        'blodgiver_egenerklering
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(9.0!, 20.0!)
         Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(734, 501)
+        Me.Controls.Add(Me.Label6)
+        Me.Controls.Add(Me.RadioButton5)
+        Me.Controls.Add(Me.RadioButton6)
+        Me.Controls.Add(Me.Label4)
+        Me.Controls.Add(Me.RadioButton3)
+        Me.Controls.Add(Me.RadioButton4)
+        Me.Controls.Add(Me.Label3)
+        Me.Controls.Add(Me.RadioButton2)
+        Me.Controls.Add(Me.RadioButton1)
+        Me.Controls.Add(Me.Label2)
+        Me.Controls.Add(Me.Label1)
+        Me.Controls.Add(Me.MenuStrip1)
+        Me.Name = "blodgiver_egenerklering"
         Me.Text = "blodgiver_egenerklering"
+        Me.MenuStrip1.ResumeLayout(False)
+        Me.MenuStrip1.PerformLayout()
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
     End Sub
+
+    Friend WithEvents MenuStrip1 As MenuStrip
+    Friend WithEvents MinSideToolStripMenuItem As ToolStripMenuItem
+    Friend WithEvents TimebestillingToolStripMenuItem As ToolStripMenuItem
+    Friend WithEvents EgenerklæringToolStripMenuItem As ToolStripMenuItem
+    Friend WithEvents Label1 As Label
+    Friend WithEvents Label2 As Label
+    Friend WithEvents RadioButton1 As RadioButton
+    Friend WithEvents RadioButton2 As RadioButton
+    Friend WithEvents RadioButton3 As RadioButton
+    Friend WithEvents RadioButton4 As RadioButton
+    Friend WithEvents Label3 As Label
+    Friend WithEvents RadioButton5 As RadioButton
+    Friend WithEvents RadioButton6 As RadioButton
+    Friend WithEvents Label4 As Label
+    Friend WithEvents Label6 As Label
 End Class
