bb1ac8a664aa5c4c050fa901c196ea271c3035b0
noralervik <noralervik@gmail.com>
2017-03-20 10:28:18 +0100

    Har satt inn ansattsider

---
+++ blodbank/ansattInnkalling.Designer.vb
@@ -0,0 +1,125 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class ansattInnkalling
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.MenuStrip1 = New System.Windows.Forms.MenuStrip()
+        Me.HjemToolStripMenuItem = New System.Windows.Forms.ToolStripMenuItem()
+        Me.ProduktoversiktToolStripMenuItem = New System.Windows.Forms.ToolStripMenuItem()
+        Me.InnkallingToolStripMenuItem = New System.Windows.Forms.ToolStripMenuItem()
+        Me.BestillingToolStripMenuItem = New System.Windows.Forms.ToolStripMenuItem()
+        Me.Button3 = New System.Windows.Forms.Button()
+        Me.Button2 = New System.Windows.Forms.Button()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.MenuStrip1.SuspendLayout()
+        Me.SuspendLayout()
+        '
+        'MenuStrip1
+        '
+        Me.MenuStrip1.ImageScalingSize = New System.Drawing.Size(24, 24)
+        Me.MenuStrip1.Items.AddRange(New System.Windows.Forms.ToolStripItem() {Me.HjemToolStripMenuItem, Me.ProduktoversiktToolStripMenuItem, Me.InnkallingToolStripMenuItem, Me.BestillingToolStripMenuItem})
+        Me.MenuStrip1.Location = New System.Drawing.Point(0, 0)
+        Me.MenuStrip1.Name = "MenuStrip1"
+        Me.MenuStrip1.Size = New System.Drawing.Size(1044, 33)
+        Me.MenuStrip1.TabIndex = 8
+        Me.MenuStrip1.Text = "MenuStrip1"
+        '
+        'HjemToolStripMenuItem
+        '
+        Me.HjemToolStripMenuItem.Name = "HjemToolStripMenuItem"
+        Me.HjemToolStripMenuItem.Size = New System.Drawing.Size(66, 29)
+        Me.HjemToolStripMenuItem.Text = "Hjem"
+        '
+        'ProduktoversiktToolStripMenuItem
+        '
+        Me.ProduktoversiktToolStripMenuItem.Name = "ProduktoversiktToolStripMenuItem"
+        Me.ProduktoversiktToolStripMenuItem.Size = New System.Drawing.Size(149, 29)
+        Me.ProduktoversiktToolStripMenuItem.Text = "Produktoversikt"
+        '
+        'InnkallingToolStripMenuItem
+        '
+        Me.InnkallingToolStripMenuItem.Name = "InnkallingToolStripMenuItem"
+        Me.InnkallingToolStripMenuItem.Size = New System.Drawing.Size(100, 29)
+        Me.InnkallingToolStripMenuItem.Text = "Innkalling"
+        '
+        'BestillingToolStripMenuItem
+        '
+        Me.BestillingToolStripMenuItem.Name = "BestillingToolStripMenuItem"
+        Me.BestillingToolStripMenuItem.Size = New System.Drawing.Size(94, 29)
+        Me.BestillingToolStripMenuItem.Text = "Bestilling"
+        '
+        'Button3
+        '
+        Me.Button3.Location = New System.Drawing.Point(529, 274)
+        Me.Button3.Margin = New System.Windows.Forms.Padding(4, 5, 4, 5)
+        Me.Button3.Name = "Button3"
+        Me.Button3.Size = New System.Drawing.Size(256, 91)
+        Me.Button3.TabIndex = 10
+        Me.Button3.Text = "Normal Innkalling"
+        Me.Button3.UseVisualStyleBackColor = True
+        '
+        'Button2
+        '
+        Me.Button2.Location = New System.Drawing.Point(241, 274)
+        Me.Button2.Margin = New System.Windows.Forms.Padding(4, 5, 4, 5)
+        Me.Button2.Name = "Button2"
+        Me.Button2.Size = New System.Drawing.Size(256, 91)
+        Me.Button2.TabIndex = 9
+        Me.Button2.Text = "Hasteinnkalling"
+        Me.Button2.UseVisualStyleBackColor = True
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.Location = New System.Drawing.Point(12, 58)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(76, 20)
+        Me.Label1.TabIndex = 11
+        Me.Label1.Text = "Innkalling"
+        '
+        'ansattInnkalling
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(9.0!, 20.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(1044, 668)
+        Me.Controls.Add(Me.Label1)
+        Me.Controls.Add(Me.Button3)
+        Me.Controls.Add(Me.Button2)
+        Me.Controls.Add(Me.MenuStrip1)
+        Me.Name = "ansattInnkalling"
+        Me.Text = "ansattInnkalling"
+        Me.MenuStrip1.ResumeLayout(False)
+        Me.MenuStrip1.PerformLayout()
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+
+    Friend WithEvents MenuStrip1 As MenuStrip
+    Friend WithEvents HjemToolStripMenuItem As ToolStripMenuItem
+    Friend WithEvents ProduktoversiktToolStripMenuItem As ToolStripMenuItem
+    Friend WithEvents InnkallingToolStripMenuItem As ToolStripMenuItem
+    Friend WithEvents BestillingToolStripMenuItem As ToolStripMenuItem
+    Friend WithEvents Button3 As Button
+    Friend WithEvents Button2 As Button
+    Friend WithEvents Label1 As Label
+End Class
