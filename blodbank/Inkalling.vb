94ffe7601fea5a8540e5424e471fb3ab863b98bf
Øistein Haugland <haugland.1193@gmail.com>
2017-04-05 10:46:55 +0200

    siste endringer fra mathias. inkalling med mail

---
+++ blodbank/Inkalling.vb
@@ -0,0 +1,22 @@
+﻿Public Class Innkalling
+
+    'Public blodgiver_id As Integer
+    Public blodgiver_id As Integer
+    Public fornavn As String
+    Public etternavn As String
+    Public epost As String
+    Public telefon As Integer
+    Public blodtype_id As Integer
+
+
+    Public Sub New(ByVal bgiver_id As Integer, I_fornavn As String, ByVal I_etternavn As String, ByVal b_epost As String, ByVal telefon As Integer, ByVal blodtype_id As Integer)
+
+        blodgiver_id = bgiver_id
+        fornavn = I_fornavn
+        etternavn = I_etternavn
+        epost = b_epost
+
+    End Sub
+
+
+End Class
--- blodbank/ansattInnkalling.vb
+++ blodbank/ansattInnkalling.vb
@@ -1,5 +1,10 @@
-﻿Public Class ansattInnkalling
+﻿Imports System.Net.Mail
+Public Class ansattInnkalling
     Dim ansattInnkallingTable As New DataTable
+    Dim Norminnkalling As New DataTable
+    Dim innkallingList As New List(Of Innkalling)
+
+
     Private Sub HjemToolStripMenuItem_Click(sender As Object, e As EventArgs) Handles HjemToolStripMenuItem.Click
         ansattStartSide.Show()
         Me.Hide()
@@ -22,62 +27,92 @@
         Me.Location = New Point(0, 0)
         Me.BackColor = Color.FromArgb(247, 247, 247)
 
-        Dim testdata = sql_sporring("SELECT blodgiver_id, fornavn, etternavn, epost, blodtype FROM Blodgiver WHERE godkjent_egenerklering = '1'")
+        Dim testdata = sql_sporring("SELECT blodgiver_id, fornavn, etternavn, epost, blodtype_id FROM Blodgiver WHERE godkjent_egenerklering = '1'")
 
 
-
-
-
-        ansattInnkallingTable = sql_sporring("SELECT * FROM Blodgiver WHERE godkjent_egenerklering = '1'")
-
-        klareBlodgivere.Items.Clear()
-
-        klareBlodgivere.Items.Add("Id" & vbTab & "Fornavn" & vbTab & "Etternavn" & vbTab & Space(2) & "Epost" & vbTab & vbTab & Space(2) & "Telefon" & Space(6) & "Blodtype")
-        klareBlodgivere.Items.Add(" ")
-
-        For Each rad In ansattInnkallingTable.Rows
-            klareBlodgivere.Items.Add(rad("blodgiver_id") & vbTab & rad("fornavn") & vbTab & rad("etternavn") & vbTab & Space(2) & rad("epost") & vbTab & Space(2) & rad("telefon") & Space(4) & rad("blodtype"))
-
-        Next
 
     End Sub
 
     Private Sub blodInfoKnapp_Click(sender As Object, e As EventArgs) Handles blodInfoKnapp.Click
-
-        ansattInnkallingTable = sql_sporring("SELECT * FROM Blodgiver WHERE godkjent_egenerklering = '1'")
-
         klareBlodgivere.Items.Clear()
-
-        klareBlodgivere.Items.Add("Id" & vbTab & "Fornavn" & vbTab & "Etternavn" & vbTab & Space(2) & "Epost" & vbTab & vbTab & Space(2) & "Telefon" & Space(6) & "Blodtype")
-        klareBlodgivere.Items.Add(" ")
-
+        ansattInnkallingTable = sql_sporring("SELECT * FROM Blodgiver WHERE godkjent_egenerklering = '1'")
         For Each rad In ansattInnkallingTable.Rows
-            klareBlodgivere.Items.Add(rad("blodgiver_id") & vbTab & rad("fornavn") & vbTab & rad("etternavn") & vbTab & Space(2) & rad("epost") & vbTab & Space(2) & rad("telefon") & Space(4) & rad("blodtype"))
 
+            Dim blodtypeStringInk
+            Select Case rad("blodtype_id")
+                Case 1
+                    blodtypeStringInk = "A+"
+                Case 2
+                    blodtypeStringInk = "A-"
+                Case 3
+                    blodtypeStringInk = "B+"
+                Case 4
+                    blodtypeStringInk = "B-"
+                Case 5
+                    blodtypeStringInk = "AB+"
+                Case 6
+                    blodtypeStringInk = "AB-"
+                Case 7
+                    blodtypeStringInk = "O+"
+                Case 8
+                    blodtypeStringInk = "O-"
+            End Select
+
+            'For Each i In ansattInnkallingTable.Rows
+            innkallingList.Add(New Innkalling(rad("blodgiver_id"), rad("fornavn"), rad("etternavn"), rad("epost"), rad("telefon"), rad("blodtype_id")))
+            klareBlodgivere.Items.Add(rad("blodgiver_id") & vbTab & rad("fornavn") & vbTab & rad("etternavn") & vbTab & Space(2) & rad("epost") & vbTab & Space(2) & rad("telefon") & Space(4) & blodtypeStringInk)
         Next
 
     End Sub
-
     Private Sub normalInnkallingKnapp_Click(sender As Object, e As EventArgs) Handles NormalInnkallingKnapp.Click
-        'Dim blodgiver As String = klareBlodgivere.SelectedItem
-        'Dim innkallingList As New List(Of String)
 
 
-        'ansattInnkallingTable = sql_sporring("SELECT * FROM Blodgiver WHERE godkjent_egenerklering = '1'")
-        'For Each rad In ansattInnkallingTable.Rows
-        '    innkallingList.Add(rad("fornavn") & " " & rad("etternavn"))
-        '    MsgBox(innkallingList)
-        'Next
+        Norminnkalling = sql_sporring("SELECT * FROM Blodgiver WHERE godkjent_egenerklering = '1'")
 
-        Dim innkallingString As String = klareBlodgivere.SelectedItem
+        Dim innkallingString
+        For Each rad In Norminnkalling.Rows
+            innkallingList.Add(New Innkalling(rad("blodgiver_id"), rad("fornavn"), rad("etternavn"), rad("epost"), rad("telefon"), rad("blodtype_id")))
+
+        Next
+
+        'Selecteditem i listbox for Â hente ut fornavn, etternavn og epost
+
+        Dim index = klareBlodgivere.SelectedIndex
+        innkallingString = innkallingList(index).fornavn & " " & innkallingList(index).etternavn
+        Dim innkallingEpost = innkallingList(index).epost
         Dim dato = Me.timeDato.Value.Date.ToString()
+        dato = Format(timeDato.Value, "dd/MM/yyyy")
         Dim tidspunkt = TidTekst.Text
-        dato = Format(timeDato.Value, "dd/mm/yyyy")
 
-        MsgBox(innkallingString & " er innkalt til time hos blodbanken. Timen er " & dato & ", klokken" & tidspunkt & " bekreft ved å trykke ok")
 
-        'MsgBox(blodgiver & " er innkalt til time hos blodbanken. Din time er " & dato & ", klokken " & tidspunkt & ". Vennligst bekreft blabla...")
-        ' Sendes på mail
+        Dim mailTekst As String = "Hei vi melder om innkalling til blodgivertime den " & dato & " kl: " & tidspunkt & ". Gi beskjed dersom du ikke kan komme. " & vbCrLf & vbCrLf & "Med vennlig hilsen" & vbCrLf & vbCrLf & "Blodbanken ved St. Olavs Hospital"
+
+        'Epost funksjonalitet for innkalling
+
+        Try
+            Dim Smtp_Server As New SmtpClient
+            Dim e_mail As New MailMessage()
+            Smtp_Server.UseDefaultCredentials = False
+            Smtp_Server.Credentials = New Net.NetworkCredential("blodbank.gruppe05@gmail.com", "blodbank1234")
+            Smtp_Server.Port = 587
+            Smtp_Server.EnableSsl = True
+            Smtp_Server.Host = "smtp.gmail.com"
+
+            e_mail = New MailMessage()
+            e_mail.From = New MailAddress("blodbank.gruppe05@gmail.com")
+            e_mail.To.Add(innkallingEpost)
+            e_mail.Subject = "Blodgivertime"
+            e_mail.IsBodyHtml = False
+            e_mail.Body = (mailTekst)
+            Smtp_Server.Send(e_mail)
+
+        Catch error_t As Exception
+            MsgBox(error_t.ToString)
+        End Try
+
+        MsgBox(innkallingString & " er innkalt til time hos blodbanken." & vbCrLf & "Timen er " & dato & ", klokken: " & tidspunkt & "." & vbCrLf)
+
+
     End Sub
 
     Private Sub RegisrerBlodgivingToolStripMenuItem_Click(sender As Object, e As EventArgs) Handles RegisrerBlodgivingToolStripMenuItem.Click
@@ -85,11 +120,5 @@
         ansattRegBlodgiving.Show()
     End Sub
 
-    Private Sub timeDato_ValueChanged(sender As Object, e As EventArgs) Handles timeDato.ValueChanged
 
-    End Sub
-
-    Private Sub klareBlodgivere_SelectedIndexChanged(sender As Object, e As EventArgs) Handles klareBlodgivere.SelectedIndexChanged
-
-    End Sub
 End Class
