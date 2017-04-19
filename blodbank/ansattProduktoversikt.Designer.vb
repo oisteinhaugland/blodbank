987a6543f1cd88831373edf79a9dfe754cef605d
Øistein Haugland <haugland.1193@gmail.com>
2017-04-19 08:52:56 +0200

    la til andreas sitt arbeid med produktoversikt

--- blodbank/ansattProduktoversikt.Designer.vb
+++ blodbank/ansattProduktoversikt.Designer.vb
@@ -22,16 +22,39 @@ Partial Class ansattProduktoversikt
     'Do not modify it using the code editor.
     <System.Diagnostics.DebuggerStepThrough()> _
     Private Sub InitializeComponent()
+        Dim ChartArea1 As System.Windows.Forms.DataVisualization.Charting.ChartArea = New System.Windows.Forms.DataVisualization.Charting.ChartArea()
+        Dim Legend1 As System.Windows.Forms.DataVisualization.Charting.Legend = New System.Windows.Forms.DataVisualization.Charting.Legend()
+        Dim Series1 As System.Windows.Forms.DataVisualization.Charting.Series = New System.Windows.Forms.DataVisualization.Charting.Series()
+        Dim ChartArea2 As System.Windows.Forms.DataVisualization.Charting.ChartArea = New System.Windows.Forms.DataVisualization.Charting.ChartArea()
+        Dim Legend2 As System.Windows.Forms.DataVisualization.Charting.Legend = New System.Windows.Forms.DataVisualization.Charting.Legend()
+        Dim Series2 As System.Windows.Forms.DataVisualization.Charting.Series = New System.Windows.Forms.DataVisualization.Charting.Series()
+        Dim ChartArea3 As System.Windows.Forms.DataVisualization.Charting.ChartArea = New System.Windows.Forms.DataVisualization.Charting.ChartArea()
+        Dim Legend3 As System.Windows.Forms.DataVisualization.Charting.Legend = New System.Windows.Forms.DataVisualization.Charting.Legend()
+        Dim Series3 As System.Windows.Forms.DataVisualization.Charting.Series = New System.Windows.Forms.DataVisualization.Charting.Series()
         Me.MenuStrip1 = New System.Windows.Forms.MenuStrip()
         Me.HjemToolStripMenuItem = New System.Windows.Forms.ToolStripMenuItem()
         Me.ProduktoversiktToolStripMenuItem = New System.Windows.Forms.ToolStripMenuItem()
         Me.InnkallingToolStripMenuItem = New System.Windows.Forms.ToolStripMenuItem()
         Me.BestillingToolStripMenuItem = New System.Windows.Forms.ToolStripMenuItem()
+        Me.RegistrerBlodgivingToolStripMenuItem = New System.Windows.Forms.ToolStripMenuItem()
         Me.Label1 = New System.Windows.Forms.Label()
         Me.Label2 = New System.Windows.Forms.Label()
         Me.Label3 = New System.Windows.Forms.Label()
-        Me.RegistrerBlodgivingToolStripMenuItem = New System.Windows.Forms.ToolStripMenuItem()
+        Me.TabControl1 = New System.Windows.Forms.TabControl()
+        Me.TabPage1 = New System.Windows.Forms.TabPage()
+        Me.chartPlasma = New System.Windows.Forms.DataVisualization.Charting.Chart()
+        Me.TabPage2 = New System.Windows.Forms.TabPage()
+        Me.chartBlodlegemer = New System.Windows.Forms.DataVisualization.Charting.Chart()
+        Me.TabPage3 = New System.Windows.Forms.TabPage()
+        Me.chartBlodplater = New System.Windows.Forms.DataVisualization.Charting.Chart()
         Me.MenuStrip1.SuspendLayout()
+        Me.TabControl1.SuspendLayout()
+        Me.TabPage1.SuspendLayout()
+        CType(Me.chartPlasma, System.ComponentModel.ISupportInitialize).BeginInit()
+        Me.TabPage2.SuspendLayout()
+        CType(Me.chartBlodlegemer, System.ComponentModel.ISupportInitialize).BeginInit()
+        Me.TabPage3.SuspendLayout()
+        CType(Me.chartBlodplater, System.ComponentModel.ISupportInitialize).BeginInit()
         Me.SuspendLayout()
         '
         'MenuStrip1
@@ -41,7 +64,7 @@ Partial Class ansattProduktoversikt
         Me.MenuStrip1.Location = New System.Drawing.Point(0, 0)
         Me.MenuStrip1.Name = "MenuStrip1"
         Me.MenuStrip1.Padding = New System.Windows.Forms.Padding(4, 1, 0, 1)
-        Me.MenuStrip1.Size = New System.Drawing.Size(623, 24)
+        Me.MenuStrip1.Size = New System.Drawing.Size(834, 24)
         Me.MenuStrip1.TabIndex = 7
         Me.MenuStrip1.Text = "MenuStrip1"
         '
@@ -68,6 +91,12 @@ Partial Class ansattProduktoversikt
         Me.BestillingToolStripMenuItem.Name = "BestillingToolStripMenuItem"
         Me.BestillingToolStripMenuItem.Size = New System.Drawing.Size(67, 22)
         Me.BestillingToolStripMenuItem.Text = "Bestilling"
+        '
+        'RegistrerBlodgivingToolStripMenuItem
+        '
+        Me.RegistrerBlodgivingToolStripMenuItem.Name = "RegistrerBlodgivingToolStripMenuItem"
+        Me.RegistrerBlodgivingToolStripMenuItem.Size = New System.Drawing.Size(125, 22)
+        Me.RegistrerBlodgivingToolStripMenuItem.Text = "Registrer Blodgiving"
         '
         'Label1
         '
@@ -99,26 +128,120 @@ Partial Class ansattProduktoversikt
         Me.Label3.TabIndex = 10
         Me.Label3.Text = "Lagerstatus:"
         '
-        'RegistrerBlodgivingToolStripMenuItem
+        'TabControl1
         '
-        Me.RegistrerBlodgivingToolStripMenuItem.Name = "RegistrerBlodgivingToolStripMenuItem"
-        Me.RegistrerBlodgivingToolStripMenuItem.Size = New System.Drawing.Size(125, 22)
-        Me.RegistrerBlodgivingToolStripMenuItem.Text = "Registrer Blodgiving"
+        Me.TabControl1.Controls.Add(Me.TabPage1)
+        Me.TabControl1.Controls.Add(Me.TabPage2)
+        Me.TabControl1.Controls.Add(Me.TabPage3)
+        Me.TabControl1.Location = New System.Drawing.Point(66, 108)
+        Me.TabControl1.Name = "TabControl1"
+        Me.TabControl1.SelectedIndex = 0
+        Me.TabControl1.Size = New System.Drawing.Size(724, 477)
+        Me.TabControl1.TabIndex = 15
+        '
+        'TabPage1
+        '
+        Me.TabPage1.Controls.Add(Me.chartPlasma)
+        Me.TabPage1.Location = New System.Drawing.Point(4, 22)
+        Me.TabPage1.Name = "TabPage1"
+        Me.TabPage1.Padding = New System.Windows.Forms.Padding(3)
+        Me.TabPage1.Size = New System.Drawing.Size(716, 451)
+        Me.TabPage1.TabIndex = 0
+        Me.TabPage1.Text = "Plasma"
+        Me.TabPage1.UseVisualStyleBackColor = True
+        '
+        'chartPlasma
+        '
+        ChartArea1.Name = "ChartArea1"
+        Me.chartPlasma.ChartAreas.Add(ChartArea1)
+        Legend1.Name = "Legend1"
+        Me.chartPlasma.Legends.Add(Legend1)
+        Me.chartPlasma.Location = New System.Drawing.Point(0, 0)
+        Me.chartPlasma.Name = "chartPlasma"
+        Series1.ChartArea = "ChartArea1"
+        Series1.Legend = "Legend1"
+        Series1.Name = "Series1"
+        Me.chartPlasma.Series.Add(Series1)
+        Me.chartPlasma.Size = New System.Drawing.Size(716, 448)
+        Me.chartPlasma.TabIndex = 0
+        Me.chartPlasma.Text = "Chart1"
+        '
+        'TabPage2
+        '
+        Me.TabPage2.Controls.Add(Me.chartBlodlegemer)
+        Me.TabPage2.Location = New System.Drawing.Point(4, 22)
+        Me.TabPage2.Name = "TabPage2"
+        Me.TabPage2.Padding = New System.Windows.Forms.Padding(3)
+        Me.TabPage2.Size = New System.Drawing.Size(716, 451)
+        Me.TabPage2.TabIndex = 1
+        Me.TabPage2.Text = "Blodlegemer"
+        Me.TabPage2.UseVisualStyleBackColor = True
+        '
+        'chartBlodlegemer
+        '
+        ChartArea2.Name = "ChartArea1"
+        Me.chartBlodlegemer.ChartAreas.Add(ChartArea2)
+        Legend2.Name = "Legend1"
+        Me.chartBlodlegemer.Legends.Add(Legend2)
+        Me.chartBlodlegemer.Location = New System.Drawing.Point(0, 0)
+        Me.chartBlodlegemer.Name = "chartBlodlegemer"
+        Series2.ChartArea = "ChartArea1"
+        Series2.Legend = "Legend1"
+        Series2.Name = "Series1"
+        Me.chartBlodlegemer.Series.Add(Series2)
+        Me.chartBlodlegemer.Size = New System.Drawing.Size(716, 448)
+        Me.chartBlodlegemer.TabIndex = 1
+        Me.chartBlodlegemer.Text = "Chart1"
+        '
+        'TabPage3
+        '
+        Me.TabPage3.Controls.Add(Me.chartBlodplater)
+        Me.TabPage3.Location = New System.Drawing.Point(4, 22)
+        Me.TabPage3.Name = "TabPage3"
+        Me.TabPage3.Padding = New System.Windows.Forms.Padding(3)
+        Me.TabPage3.Size = New System.Drawing.Size(716, 451)
+        Me.TabPage3.TabIndex = 2
+        Me.TabPage3.Text = "Blodplater"
+        Me.TabPage3.UseVisualStyleBackColor = True
+        '
+        'chartBlodplater
+        '
+        ChartArea3.Name = "ChartArea1"
+        Me.chartBlodplater.ChartAreas.Add(ChartArea3)
+        Legend3.Name = "Legend1"
+        Me.chartBlodplater.Legends.Add(Legend3)
+        Me.chartBlodplater.Location = New System.Drawing.Point(0, 0)
+        Me.chartBlodplater.Name = "chartBlodplater"
+        Series3.ChartArea = "ChartArea1"
+        Series3.Legend = "Legend1"
+        Series3.Name = "Series1"
+        Me.chartBlodplater.Series.Add(Series3)
+        Me.chartBlodplater.Size = New System.Drawing.Size(716, 448)
+        Me.chartBlodplater.TabIndex = 1
+        Me.chartBlodplater.Text = "Chart1"
         '
         'ansattProduktoversikt
         '
         Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
         Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
-        Me.ClientSize = New System.Drawing.Size(623, 406)
+        Me.ClientSize = New System.Drawing.Size(834, 580)
+        Me.Controls.Add(Me.TabControl1)
         Me.Controls.Add(Me.Label3)
         Me.Controls.Add(Me.Label2)
         Me.Controls.Add(Me.Label1)
         Me.Controls.Add(Me.MenuStrip1)
-        Me.Margin = New System.Windows.Forms.Padding(2, 2, 2, 2)
+        Me.Margin = New System.Windows.Forms.Padding(2)
         Me.Name = "ansattProduktoversikt"
         Me.Text = "ansattProduktoversikt"
         Me.MenuStrip1.ResumeLayout(False)
         Me.MenuStrip1.PerformLayout()
+        Me.TabControl1.ResumeLayout(False)
+        Me.TabPage1.ResumeLayout(False)
+        CType(Me.chartPlasma, System.ComponentModel.ISupportInitialize).EndInit()
+        Me.TabPage2.ResumeLayout(False)
+        CType(Me.chartBlodlegemer, System.ComponentModel.ISupportInitialize).EndInit()
+        Me.TabPage3.ResumeLayout(False)
+        CType(Me.chartBlodplater, System.ComponentModel.ISupportInitialize).EndInit()
         Me.ResumeLayout(False)
         Me.PerformLayout()
 
@@ -133,4 +256,11 @@ Partial Class ansattProduktoversikt
     Friend WithEvents Label2 As Label
     Friend WithEvents Label3 As Label
     Friend WithEvents RegistrerBlodgivingToolStripMenuItem As ToolStripMenuItem
+    Friend WithEvents TabControl1 As TabControl
+    Friend WithEvents TabPage1 As TabPage
+    Friend WithEvents TabPage2 As TabPage
+    Friend WithEvents TabPage3 As TabPage
+    Friend WithEvents chartPlasma As DataVisualization.Charting.Chart
+    Friend WithEvents chartBlodlegemer As DataVisualization.Charting.Chart
+    Friend WithEvents chartBlodplater As DataVisualization.Charting.Chart
 End Class
--- blodbank/ansattProduktoversikt.vb
+++ blodbank/ansattProduktoversikt.vb
@@ -1,4 +1,7 @@
-﻿Public Class ansattProduktoversikt
+﻿
+Imports System.Windows.Forms.DataVisualization.Charting
+
+Public Class ansattProduktoversikt
     Private Sub HjemToolStripMenuItem_Click(sender As Object, e As EventArgs) Handles HjemToolStripMenuItem.Click
         ansattStartSide.Show()
         Me.Hide()
@@ -17,5 +20,178 @@
     Private Sub RegistrerBlodgivingToolStripMenuItem_Click(sender As Object, e As EventArgs) Handles RegistrerBlodgivingToolStripMenuItem.Click
         Me.Hide()
         ansattRegBlodgiving.Show()
+    End Sub
+
+    Private Sub ansattProduktoversikt_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+
+        Me.WindowState = FormWindowState.Maximized
+        Me.BackColor = Color.FromArgb(247, 247, 247)
+
+        Dim prodOversikt = sql_sporring("SELECT * FROM Blodlager")
+
+        Dim antallPlasma As Integer
+        Dim antallBlodlegemer As Integer
+        Dim antallBlodplater As Integer
+
+        Dim PlasmaAntallAp, PlasmaAntallAm, PlasmaAntallBp, PlasmaAntallBm, PlasmaAntallABp, PlasmaAntallABm, PlasmaAntallOp, PlasmaAntallOm As Integer
+        Dim BlodLegemerAntallAp, BlodLegemerAntallAm, BlodLegemerAntallBp, BlodLegemerAntallBm, BlodLegemerAntallABp, BlodLegemerAntallABm, BlodLegemerAntallOp, BlodLegemerAntallOm As Integer
+        Dim BlodPlaterAntallAp, BlodPlaterAntallAm, BlodPlaterAntallBp, BlodPlaterAntallBm, BlodPlaterAntallABp, BlodPlaterAntallABm, BlodPlaterAntallOp, BlodPlaterAntallOm As Integer
+
+
+        Dim plasmaOversikt = sql_sporring("SELECT * FROM Blodlager WHERE blodegenskap_id = 3")
+        For Each rad In plasmaOversikt.Rows
+            Select Case rad("blodtype_id")
+                Case 1
+                    PlasmaAntallAp += 1
+                Case 2
+                    PlasmaAntallAm += 1
+                Case 3
+                    PlasmaAntallBp += 1
+                Case 4
+                    PlasmaAntallBm += 1
+                Case 5
+                    PlasmaAntallABp += 1
+                Case 6
+                    PlasmaAntallABm += 1
+                Case 7
+                    PlasmaAntallOp += 1
+                Case 8
+                    PlasmaAntallOm += 1
+            End Select
+        Next
+        Dim blodplaterOversikt = sql_sporring("SELECT * FROM Blodlager WHERE blodegenskap_id = 2")
+        For Each rad In blodplaterOversikt.Rows
+            Select Case rad("blodtype_id")
+                Case 1
+                    BlodPlaterAntallAp += 1
+                Case 2
+                    BlodPlaterAntallAm += 1
+                Case 3
+                    BlodPlaterAntallBp += 1
+                Case 4
+                    BlodPlaterAntallBm += 1
+                Case 5
+                    BlodPlaterAntallABp += 1
+                Case 6
+                    BlodPlaterAntallABm += 1
+                Case 7
+                    BlodPlaterAntallOp += 1
+                Case 8
+                    BlodPlaterAntallOm += 1
+            End Select
+        Next
+
+        Dim blodlegemerOversikt = sql_sporring("SELECT * FROM Blodlager WHERE blodegenskap_id = 1")
+        For Each rad In blodlegemerOversikt.Rows
+            Select Case rad("blodtype_id")
+                Case 1
+                    BlodLegemerAntallAp += 1
+                Case 2
+                    BlodLegemerAntallAm += 1
+                Case 3
+                    BlodLegemerAntallBp += 1
+                Case 4
+                    BlodLegemerAntallBm += 1
+                Case 5
+                    BlodLegemerAntallABp += 1
+                Case 6
+                    BlodLegemerAntallABm += 1
+                Case 7
+                    BlodLegemerAntallOp += 1
+                Case 8
+                    BlodLegemerAntallOm += 1
+            End Select
+        Next
+
+
+        For Each rad In prodOversikt.Rows
+            If rad("blodegenskap_id") = 3 Then
+                antallPlasma += 1
+            ElseIf rad("blodegenskap_id") = 2 Then
+                antallBlodplater += 1
+            Else
+                antallBlodlegemer += 1
+            End If
+
+        Next
+        chartPlasma.Series.Clear()
+        chartPlasma.Series.Add("Plasma oversikt")
+
+        Dim pOversikt As New Series
+        pOversikt.XAxisType = AxisType.Primary
+        pOversikt.SmartLabelStyle.Enabled = False
+        pOversikt.LabelAngle = 45
+        pOversikt.IsVisibleInLegend = False
+        'pOversikt.ChartType = SeriesChartType.Bar
+        pOversikt.XValueType = ChartValueType.String
+
+        With pOversikt.Points
+            .AddXY("A+", PlasmaAntallAp)
+            .AddXY("A-", PlasmaAntallAm)
+            .AddXY("B+", PlasmaAntallBp)
+            .AddXY("B-", PlasmaAntallBm)
+            .AddXY("AB+", PlasmaAntallABp)
+            .AddXY("AB-", PlasmaAntallABm)
+            .AddXY("O+", PlasmaAntallOp)
+            .AddXY("O-", PlasmaAntallOm)
+        End With
+
+        chartPlasma.Series.Add(pOversikt)
+        '__________________________________________________________
+        chartBlodplater.Series.Clear()
+        chartBlodplater.Series.Add("Blodplater oversikt")
+
+        Dim blodplatOversikt As New Series
+        blodplatOversikt.XAxisType = AxisType.Primary
+        blodplatOversikt.SmartLabelStyle.Enabled = False
+        blodplatOversikt.LabelAngle = 45
+        blodplatOversikt.IsVisibleInLegend = False
+        'blodplatOversikt.ChartType = SeriesChartType.Bar
+        blodplatOversikt.XValueType = ChartValueType.String
+
+        With blodplatOversikt.Points
+            .AddXY("A+", BlodPlaterAntallAp)
+            .AddXY("A-", BlodPlaterAntallAm)
+            .AddXY("B+", BlodPlaterAntallBp)
+            .AddXY("B-", BlodPlaterAntallBm)
+            .AddXY("AB+", BlodPlaterAntallABp)
+            .AddXY("AB-", BlodPlaterAntallABm)
+            .AddXY("O+", BlodPlaterAntallOp)
+            .AddXY("O-", BlodPlaterAntallOm)
+        End With
+
+        chartBlodplater.Series.Add(blodplatOversikt)
+        '__________________________________________________________
+        chartBlodlegemer.Series.Clear()
+        chartBlodlegemer.Series.Add("Blodlegemer oversikt")
+
+        Dim blodlegOversikt As New Series
+        blodlegOversikt.XAxisType = AxisType.Primary
+        blodlegOversikt.SmartLabelStyle.Enabled = False
+        blodlegOversikt.LabelAngle = 45
+        blodlegOversikt.IsVisibleInLegend = False
+        'blodlegOversikt.ChartType = SeriesChartType.Bar
+        blodlegOversikt.XValueType = ChartValueType.String
+
+        With blodlegOversikt.Points
+            .AddXY("A+", BlodLegemerAntallAp)
+            .AddXY("A-", BlodLegemerAntallAm)
+            .AddXY("B+", BlodLegemerAntallBp)
+            .AddXY("B-", BlodLegemerAntallBm)
+            .AddXY("AB+", BlodLegemerAntallABp)
+            .AddXY("AB-", BlodLegemerAntallABm)
+            .AddXY("O+", BlodLegemerAntallOp)
+            .AddXY("O-", BlodLegemerAntallOm)
+        End With
+
+        chartBlodlegemer.Series.Add(blodlegOversikt)
+    End Sub
+
+    Private Sub chartBlodLager_Click(sender As Object, e As EventArgs) Handles chartPlasma.Click
+
+    End Sub
+
+    Private Sub chartBlodlegemer_Click(sender As Object, e As EventArgs) Handles chartBlodlegemer.Click
+
     End Sub
 End Class
